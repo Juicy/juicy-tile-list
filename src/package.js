8d7d823d0ac7852042de2f47654b097acf23b598
Tomek Wytrebowicz <tomalecpub@gmail.com>
2015-12-02 02:16:57 +0100

    1.0.1

--- src/package.js
+++ src/package.js
@@ -1,6 +1,6 @@
 /**
  * Package
- * version: 1.0.0
+ * version: 1.0.1
  */
 (function( scope, Packer ){
 "use strict";
--- src/packer.js
+++ src/packer.js
@@ -1,7 +1,7 @@
 /**
  * Packer
  * bin-packing algorithm
- * version: 1.0.0
+ * version: 1.0.1
  */
 (function( scope ){
 "use strict";
